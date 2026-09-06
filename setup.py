f8f89c291cf5c48f437df23c9828c99a8f7711a5
Sumit07125 <sumitmali07125@gmail.com>
2026-09-06 13:50:39 +0530

    Add setup.py and requirements.txt for package management; initialize __init__.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages,setup
+from typing import List
+
+
+
+HYPEN_E_DOT = '-e .'
+def ger_requirements(file_path:str)->List[str]:
+    ''' 
+    this funtion will return the requirements of list
+    '''
+    requirements = []
+
+    with open(file_path) as file_obj:
+        requirements = file_obj.readline
+        requirements = [req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+            
+        return requirements
+
+
+setup(
+    name='ML_End_to_End_Pipeline',
+    author="Sumit Mali",
+    version= "0.0.1",
+    author_email= 'sumitmali07125@gmail.com',
+    packages= find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
