aa58eb8af4662221bc61ac17bda2337ccda493c5
Sumit07125 <sumitmali07125@gmail.com>
2026-09-11 12:46:06 +0530

    All Pipe line is ready

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,5 +1,8 @@
 import os 
 import sys
+
+os.environ["LOKY_MAX_CPU_COUNT"] = "8"
+
 from src.logger import logging
 from src.exception import CustomException
 import pandas as pd
@@ -7,6 +10,7 @@ import pandas as pd
 
 from src.components.data_transformation import DataTransformation,DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainer,ModelTrainerConfig
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
@@ -47,4 +51,7 @@ if __name__ == "__main__":
     obj = DataIngestion()
     train_data,test_data = obj.initiate_data_ingestion()
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_ =data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
